6553017b50b3ac6a9205bc01cb8632e1ff019d7e
Lucas Lima <lucas.m.s.lima@gmail.com>
2014-08-18 15:27:59 -0500

    Changed description of the license

--- wp-content/themes/arcade-basic-jr/footer.php
+++ wp-content/themes/arcade-basic-jr/footer.php
@@ -14,7 +14,7 @@ $bavotasan_theme_options = bavotasan_theme_options();
 		<div id="footer-content" class="container">
 			<div class="row">
 				<div class="copyright col-lg-12">
-					<span class="pull-left"><?php printf( __( '<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">%s</span> is licensed under <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.', 'arcade' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
+					<span class="pull-left"><?php printf( __( '<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">%s</span> is licensed under <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.', 'arcade' ), ' <a href="' . home_url() . '">' . get_bloginfo( 'name' ) .'</a>' ); ?></span>
 					<span class="credit-link pull-right"><i class="fa fa-leaf"></i><?php printf( __( 'Designed by %s.', 'arcade' ), '<a href="https://themes.bavotasan.com/">bavotasan.com</a>' ); ?></span>
 				</div><!-- .col-lg-12 -->
 			</div><!-- .row -->
