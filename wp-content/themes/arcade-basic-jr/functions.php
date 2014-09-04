be287a31d3bbc7371b15c9ab3bbd6b0912d76ace
Lucas Lima <lucas.m.s.lima@gmail.com>
2014-09-04 13:37:12 -0500

    Included function to add favicon icon in function.php. close #20

--- wp-content/themes/arcade-basic-jr/functions.php
+++ wp-content/themes/arcade-basic-jr/functions.php
@@ -10,3 +10,16 @@ function load_fonts() {
         }
     
     add_action('wp_print_styles', 'load_fonts');
+
+/**
+ * Custom Function to Include a favicon
+ *
+ * @since 1.0
+ */
+function favicon_link() {
+    echo '<link rel="shortcut icon" href="http://canadawebpros.ca/projects/axevancouver/wp-content/uploads/2014/03/axe-capoeira-fevicone.png">' . "\n";
+	echo '<link rel="icon" href="http://canadawebpros.ca/projects/axevancouver/wp-content/uploads/2014/03/axe-capoeira-fevicone1.png" type="image/gif">' . "\n";
+	echo '<link rel="icon" href="http://canadawebpros.ca/projects/axevancouver/wp-content/uploads/2014/03/axe-capoeira-fevicone2.png" type="image/png">' . "\n";
+	echo '<link rel="apple-touch-icon" href="http://canadawebpros.ca/projects/axevancouver/wp-content/uploads/2014/03/axe-capoeira-fevicone3.png">' . "\n";
+}
+add_action( 'wp_head', 'favicon_link' );
