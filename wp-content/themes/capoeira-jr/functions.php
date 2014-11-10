129ac4eab2cde510e5a9f1099851d05353a2c285
Lucas Lima <lucas.m.s.lima@gmail.com>
2014-11-09 18:50:42 -0600

    changed title fonts to ubuntu. Close #25

--- wp-content/themes/capoeira-jr/functions.php
+++ wp-content/themes/capoeira-jr/functions.php
@@ -5,7 +5,7 @@
  * @since 1.0
  */
 function load_fonts() {
-            wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Permanent+Marker');
+            wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Ubuntu');
             wp_enqueue_style( 'googleFonts');
         }
     
