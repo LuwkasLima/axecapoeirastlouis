caf5c096521fd0c6133dd15d2037faa4ecc2c7b1
Lucas Lima <lucas.m.s.lima@gmail.com>
2015-02-08 10:18:31 -0600

    Created template for Payment page - Not working with widgets yet

--- wp-content/themes/capoeira-jr/functions.php
+++ wp-content/themes/capoeira-jr/functions.php
@@ -23,3 +23,25 @@ function favicon_link() {
 	echo '<link rel="apple-touch-icon" href="http://canadawebpros.ca/projects/axevancouver/wp-content/uploads/2014/03/axe-capoeira-fevicone3.png">' . "\n";
 }
 add_action( 'wp_head', 'favicon_link' );
+
+/**
+ * Custom Function to Include a new widget area
+ *
+ * @since 1.0
+ */
+ add_action( 'widgets_init', 'capoeirajr_widgets_init' );
+ if ( ! function_exists( 'bavotasan_widgets_init' ) ) :
+function capoeirajr_widgets_init() {
+	$bavotasan_theme_options = bavotasan_theme_options();
+
+	register_sidebar( array(
+		'name' => __( 'Paypal area', 'arcade' ),
+		'id' => 'payment-page-area',
+		'description' => __( 'Area on the payment page designed specifically for pay options buttons. Add at least one widget to make it appear.', 'arcade' ),
+		'before_widget' => '<aside id="%1$s" class="payment-widget col-md-3 %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="payment-widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+endif; // capoeirajr_widgets_init
